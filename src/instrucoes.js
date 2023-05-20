d6fa6582fbeddf2bfbfcd3f7aa381fddc5884c83
TaylorNAlbarnaz <veloxgods@gmail.com>
2023-05-20 11:41:40 -0300

    Rebalanceamento nas duracoes

--- src/instrucoes.js
+++ src/instrucoes.js
@@ -2,49 +2,49 @@ const Instrucoes = {
     "1": {
         texto: "Vire a direita na preferencial",
         infracoes: [8, 5],
-        duracao: 7,
+        duracao: 12,
         voz: "direita-pref"
     },
     "2": {
         texto: "Vire a direita com placa de pare",
         infracoes: [8, 2, 3, 5, 4],
-        duracao: 10,
+        duracao: 25,
         voz: "direita"
     },
     "3": {
         texto: "Vire a direita em via de mão unica",
         infracoes: [8, 2, 3, 5, 4],
-        duracao: 12,
+        duracao: 17,
         voz: "direita-unica"
     },
     "4": {
         texto: "Vire a esquerda na preferencial",
         infracoes: [7, 6],
-        duracao: 7,
+        duracao: 12,
         voz: "esquerda-pref"
     },
     "5": {
         texto: "Vire a esquerda com placa de pare",
         infracoes: [7, 2, 3, 6, 4],
-        duracao: 10,
+        duracao: 25,
         voz: "esquerda"
     },
     "6": {
         texto: "Vire a esquerda em via de mão única",
         infracoes: [7, 1, 12, 2, 3, 6, 4],
-        duracao: 14,
+        duracao: 20,
         voz: "esquerda-unica"
     },
     "7": {
         texto: "Estacione a frente",
         infracoes: [8, 10, 17],
-        duracao: 19,
+        duracao: 20,
         voz: "estacionar"
     },
     "8": {
         texto: "Saia do estacionamento",
         infracoes: [1, 7, 11, 15, 4],
-        duracao: 14,
+        duracao: 15,
         voz: "sair"
     },
     "9": {

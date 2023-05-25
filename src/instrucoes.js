8a90838c71bf97cb53ee2c3f6da954bdff783514
TaylorNAlbarnaz <veloxgods@gmail.com>
2023-05-25 08:34:44 -0300

    Alteraçõs nas Instruções

--- src/instrucoes.js
+++ src/instrucoes.js
@@ -7,13 +7,13 @@ const Instrucoes = {
     },
     "2": {
         texto: "Vire a direita com placa de pare",
-        infracoes: [8, 2, 3, 5, 4],
+        infracoes: [1, 8, 2, 3, 5, 4],
         duracao: 22,
         voz: "direita"
     },
     "3": {
         texto: "Vire a direita em via de mão unica",
-        infracoes: [8, 2, 3, 5, 4],
+        infracoes: [8, 5],
         duracao: 17,
         voz: "direita-unica"
     },
@@ -25,31 +25,31 @@ const Instrucoes = {
     },
     "5": {
         texto: "Vire a esquerda com placa de pare",
-        infracoes: [7, 2, 3, 6, 4],
+        infracoes: [1, 7, 2, 3, 6, 4],
         duracao: 22,
         voz: "esquerda"
     },
     "6": {
         texto: "Vire a esquerda em via de mão única",
-        infracoes: [7, 1, 12, 2, 3, 6, 4],
+        infracoes: [1, 7, 1, 12, 6],
         duracao: 19,
         voz: "esquerda-unica"
     },
     "7": {
         texto: "Estacione a frente",
-        infracoes: [8, 10, 17],
-        duracao: 20,
+        infracoes: [1, 8, 10, 17],
+        duracao: 24,
         voz: "estacionar"
     },
     "8": {
         texto: "Saia do estacionamento",
         infracoes: [1, 7, 11, 15, 4],
-        duracao: 15,
+        duracao: 19,
         voz: "sair"
     },
     "9": {
         texto: "Faça um retorno de 3 pontas",
-        infracoes: [7, 14, 3, 1],
+        infracoes: [1, 7, 14, 3],
         duracao: 30,
         voz: "tres-pontas"
     },
@@ -67,7 +67,7 @@ const Instrucoes = {
     },
     "12": {
         texto: "Saia para fazer a baliza",
-        infracoes: [16, 7, 11, 15, 3],
+        infracoes: [1, 16, 7, 11, 15, 3],
         duracao: 18,
         voz: "comecar-baliza"
     }
